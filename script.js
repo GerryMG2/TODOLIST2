5d8347668c14714a805130fd6d288b76db9c8843
GerryMG2 <37318994+GerryMG2@users.noreply.github.com>
2018-10-26 18:38:20 -0600

    VERSION 1

---
+++ script.js
@@ -0,0 +1,51 @@
+window.onload = init;
+
+function init() {
+    let todoList = {
+        listHTML: document.getElementById("todoList"),
+        listTask: [],
+        add(task, priority = false) {
+            let element = document.createElement("li");
+            element.innerText = task ;
+            element.innerHTML += " <input type='checkbox'/>";
+            /*element.addEventListener("click", () => {
+               let parent = element.parentNode;
+               if(parent){
+                   parent.removeChild(element);
+               }
+            });*/
+            
+           // Añadir un boton para marcar de finalizado
+           // Elmine de la lista
+
+            if (priority) {
+                this.listTask.unshift({
+                    element,
+                    task
+                });
+                this.listHTML.insertBefore(element, this.listHTML.childNodes[0]);
+            } else {
+                this.listTask.push({
+                    element,
+                    task
+                });
+                this.listHTML.appendChild(element);
+            }
+        }
+    }
+
+    let form = document.managerTask;
+    form.addEventListener("submit", (evt) => {
+        evt.preventDefault();
+        let task = form.task.value;
+
+        let validTask = /.{2,}/;
+        if (!validTask.test(task)) {
+            console.log("Ingrese una descripcion clara");
+            return false;
+        }
+
+        todoList.add(task, form.important.checked);
+
+    });
+}
